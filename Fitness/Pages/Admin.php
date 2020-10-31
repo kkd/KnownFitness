ba863595ff9fa07158f3bf294dcc85e0e887393b
Takeshi Kakeda <kakeda@zensow.jp>
2020-10-31 17:24:35 +0900

    Fix call classes.

--- Fitness/Pages/Admin.php
+++ Fitness/Pages/Admin.php
@@ -14,7 +14,7 @@ namespace IdnoPlugins\Fitness\Pages {
         function getContent() {
             
             $this->adminGatekeeper(); // Admins only
-            $t = \Idno\Core\Idno::site()->template();
+            $t = Idno::site()->template();
             $body = $t->draw('admin/fitness');
             $t->__(array('title' => 'Fitness settings', 'body' => $body))->drawPage();
         }
@@ -23,25 +23,32 @@ namespace IdnoPlugins\Fitness\Pages {
 
             $this->adminGatekeeper(); // Admins only
 
-            $metric = $this->getInput('metric');
-            $mapdata = $this->getInput('mapdata');
-            $weight = $this->getInput('weight');
-            $height = $this->getInput('height');
+            try {
 
-            if ($site) {}
-                \Idno\Core\site()->config->config['fitness'] = array(
-                    'metric'=>$metric,
-                    'mapdata' => $mapdata,
-                    'weight' => $weight,
-                    'height' => $height
-                );
-                \Idno\Core\site()->config()->save();
-                \Idno\Core\site()->session()->addMessage('Your Fitness settings were saved.');
+                $metric = $this->getInput('metric');
+                $mapdata = $this->getInput('mapdata');
+                $weight = $this->getInput('weight');
+                $height = $this->getInput('height');
+
+                if ($site) {}
+                    \Idno\Core\site()->config->config['fitness'] = array(
+                        'metric'=>$metric,
+                        'mapdata' => $mapdata,
+                        'weight' => $weight,
+                        'height' => $height
+                    );
+                    \Idno\Core\site()->config()->save();
+                    \Idno\Core\site()->session()->addMessage('Your Fitness settings were saved.');
+                }
+                else {
+                    \Idno\Core\site()->session()->addErrorMessage('You must enter the site URL of the Known site you want to cross post to');
+                }
+            } catch (\Exception $e) {
+                \Idno\Core\site()->session()->addErrorMessage($e->getMessage());
             }
-            else {
-                \Idno\Core\site()->session()->addErrorMessage('You must enter the site URL of the Known site you want to cross post to');
-            }
-            $this->forward(\Idno\Core\site()->config()->getDisplayURL() . 'admin/fitness/');
+
+            $this->forward(Idno::site()->config()->getDisplayURL() . 'admin/fitness/');
+
         }
 
     }
