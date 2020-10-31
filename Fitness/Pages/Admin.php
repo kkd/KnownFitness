8fbc5c92986a94b74cbb644221d1418fbdc046a5
Takeshi Kakeda <kakeda@zensow.jp>
2020-10-31 17:38:13 +0900

    Remove getDisplayUrl()

--- Fitness/Pages/Admin.php
+++ Fitness/Pages/Admin.php
@@ -12,9 +12,9 @@ namespace IdnoPlugins\Fitness\Pages {
     class Admin extends \Idno\Common\Page {
 
         function getContent() {
-            
+
             $this->adminGatekeeper(); // Admins only
-            $t = \Idno\Core\Idno::site()->template();
+            $t = \Idno\Core\site()->template();
             $body = $t->draw('admin/fitness');
             $t->__(array('title' => 'Fitness settings', 'body' => $body))->drawPage();
         }
@@ -31,23 +31,24 @@ namespace IdnoPlugins\Fitness\Pages {
                 $height = $this->getInput('height');
 
                 if ($site) {
-                    \Idno\Core\Idno::site()->config->config['fitness'] = array(
+                    \Idno\Core\site()->config->config['fitness'] = array(
                         'metric'=>$metric,
                         'mapdata' => $mapdata,
                         'weight' => $weight,
                         'height' => $height
                     );
-                    \Idno\Core\Idno::site()->config()->save();
-                    \Idno\Core\Idno::site()->session()->addMessage('Your Fitness settings were saved.');
+                    \Idno\Core\site()->config()->save();
+                    \Idno\Core\site()->session()->addMessage('Your Fitness settings were saved.');
                 }
                 else {
-                    \Idno\Core\Idno::site()->session()->addErrorMessage('You must enter the site URL of the Known site you want to cross post to');
+                    \Idno\Core\site()->session()->addErrorMessage('You must enter the site URL of the Known site you want to cross post to');
                 }
             } catch (\Exception $e) {
-                \Idno\Core\Idno::site()->session()->addErrorMessage($e->getMessage());
+                \Idno\Core\site()->session()->addErrorMessage($e->getMessage());
             }
 
-            $this->forward(\Idno\Core\Idno::site()->config()->getDisplayURL() . 'admin/fitness/');
+            // $this->forward(Idno::site()->config()->getDisplayURL() . 'admin/fitness/');
+            $this->forward('admin/fitness/');            
 
         }
 
